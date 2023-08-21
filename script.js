3a2d4524b72dd1b69a824198d70a1d3c90e5bf1b
RPWang <58319077+wangrp1997@users.noreply.github.com>
2023-08-22 00:07:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,2 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const heart = document.createElement("div");
-  heart.classList.add("heart");
-  document.body.appendChild(heart);
-
-  heart.addEventListener("animationend", () => {
-    heart.remove();
-  });
-
-  document.addEventListener("click", (event) => {
-    const heartClone = heart.cloneNode();
-    const size = Math.random() * 100 + 50;
-    const x = event.clientX - size / 2;
-    const y = event.clientY - size / 2;
-
-    heartClone.style.width = `${size}px`;
-    heartClone.style.height = `${size}px`;
-    heartClone.style.left = `${x}px`;
-    heartClone.style.top = `${y}px`;
-
-    document.body.appendChild(heartClone);
-
-    setTimeout(() => {
-      heartClone.remove();
-    }, 3000);
-  });
-});
+// 这里是空白的，因为我们在 HTML 中已经包含了动画效果的 CSS
+// 如果需要其他 JavaScript 逻辑，可以在这里添加
